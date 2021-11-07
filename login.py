7e91d1f8bf6b49ac401f6aec4ffd62ae0afa9265
nazarsokal <nazarsokalch@gmail.com>
2021-11-07 10:48:14 +0200

    login system
    New features:
    1) Created ready login system

--- login.py
+++ login.py
@@ -1,6 +1,10 @@
+from logging import PlaceHolder
+from os import close, name
 import mysql.connector
 from pywebio.input import *
-from pywebio.output import put_text
+from pywebio.output import close_popup, output, put_button, put_markdown, put_scrollable, put_table, put_text
+from functools import partial
+import asyncio
 
 conn = mysql.connector.connect(
     user = 'root',
@@ -10,13 +14,43 @@ conn = mysql.connector.connect(
 )
 
 mycurs = conn.cursor()
+put_markdown("Login into your account to start messaging")
+user_login = input_group("Login",
+[
+    input('Input your nickname', name='nickname', required=True),
+    input('Input your password', name='password', required=True, type=PASSWORD)
+])
+put_button("Don't have an account", onclick=lambda: react())
+name = user_login['nickname']
+password = user_login['password']
+mycurs.execute("SELECT login,password FROM user_db")
+myresult = mycurs.fetchall()
+for i in myresult:
+    name_from_db = i[0]
+    password_from_db = i[1]
+    if name_from_db == name and password_from_db == password:
+        print("Good")
+    else:
+        put_markdown("Your login or password is incorrect(if you don't have account,press upper button)")
 
-def autorization():
-    login = input("Enter your login: ", type=TEXT)
-    password = input("Enter your password: ", type=PASSWORD)
     
-if __name__ == '__main__':
-    autorization()
+    
+def react():
+    info = input_group("Register",[
+        input('Input your nickname', name='nickname', validate=reg_check),
+        input('Input your password', name='password', type=PASSWORD)
+        ])
+
+def reg_check(name):
+    for i in mycurs.execute("SELECT * FROM user_db(id,login,password)"):
+        if name == i:
+            put_markdown("This name is already exists")
+        else:
+            put_markdown("All good")
+
+# if __name__ == "__main__":
+#     autorization()
+
 conn.close()
 
 
