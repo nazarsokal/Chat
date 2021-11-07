56fd5826a576c5601deaba20dda1adb8e43d8ed5
nazarsokal <nazarsokalch@gmail.com>
2021-11-07 13:20:36 +0200

    Updated login system
    New features:
    1) If user activated checkbox - "Stay loged in", his data will be put on inputs
    2) Ip address sotring system

--- login.py
+++ login.py
@@ -1,10 +1,14 @@
 from logging import PlaceHolder
 from os import close, name
+from typing import Text
 import mysql.connector
 from pywebio.input import *
 from pywebio.output import close_popup, output, put_button, put_markdown, put_scrollable, put_table, put_text
 from functools import partial
 import asyncio
+import socket
+
+ip_addres = socket.gethostbyname(socket.gethostname())
 
 conn = mysql.connector.connect(
     user = 'root',
@@ -12,44 +16,61 @@ conn = mysql.connector.connect(
     host = 'localhost',
     database = 'db_for_chat'
 )
-
 mycurs = conn.cursor()
-put_markdown("Login into your account to start messaging")
-user_login = input_group("Login",
-[
-    input('Input your nickname', name='nickname', required=True),
-    input('Input your password', name='password', required=True, type=PASSWORD)
-])
-put_button("Don't have an account", onclick=lambda: react())
-name = user_login['nickname']
-password = user_login['password']
-mycurs.execute("SELECT login,password FROM user_db")
+
+def reaction(value):
+    pass
+
+mycurs.execute("SELECT * FROM user_db WHERE ip = %s",(ip_addres,))
 myresult = mycurs.fetchall()
 for i in myresult:
-    name_from_db = i[0]
-    password_from_db = i[1]
-    if name_from_db == name and password_from_db == password:
-        print("Good")
-    else:
-        put_markdown("Your login or password is incorrect(if you don't have account,press upper button)")
-
-    
-    
-def react():
-    info = input_group("Register",[
-        input('Input your nickname', name='nickname', validate=reg_check),
-        input('Input your password', name='password', type=PASSWORD)
+    name_from_db = i[1]
+    password_from_db = i[2]
+    ip_from_db = i[3]
+    isLoged_from_db = i[4]
+    if ip_addres == ip_from_db and isLoged_from_db == 'Staying loged in':
+        user_login_isLoged = input_group("Login",
+        [
+            input('Input your nickname', name='nickname', value=name_from_db, required=True, action=('Register', reaction)),
+            input('Input your password', name='password', value=password_from_db, type=PASSWORD, required=True),
+            checkbox(' ', name='isLoged', options=['Staying loged in'], value='Staying loged in')
         ])
-
-def reg_check(name):
-    for i in mycurs.execute("SELECT * FROM user_db(id,login,password)"):
-        if name == i:
-            put_markdown("This name is already exists")
-        else:
-            put_markdown("All good")
-
-# if __name__ == "__main__":
-#     autorization()
+        nickname_is_LogedIn = user_login_isLoged['nickname']
+        password_is_LogedIn = user_login_isLoged['password']
+        checkbox_is_LogedIn = user_login_isLoged['isLoged']
+        try:
+            if nickname_is_LogedIn == name_from_db and password_is_LogedIn == password_from_db and checkbox_is_LogedIn[0] == isLoged_from_db:
+                print("All good")
+            else:
+                put_markdown("Your login or password is incorrect(if you don't have account,press upper button)")
+        except Exception as e:
+            if nickname_is_LogedIn == name_from_db and password_is_LogedIn == password_from_db and repr(e) == "IndexError('list index out of range')": 
+                mycurs.execute("UPDATE user_db SET is_loged = 'No' WHERE ip = %s",(ip_addres,))
+                conn.commit()
+            #put_markdown("%s %s %s" % (nickname_is_LogedIn,password_is_LogedIn,checkbox_is_LogedIn[0])) 
+    else:
+        user_login = input_group("Login",
+        [
+            input('Input your nickname', name='nickname', required=True, action=('Register', reaction)),
+            input('Input your password', name='password', type=PASSWORD, required=True),
+            checkbox(' ', name='isLoged', options=['Staying loged in'])
+        ])
+        nickname = user_login['nickname']
+        password = user_login['password']
+        checkbx = user_login['isLoged']
+        try:
+            if nickname == name_from_db and password == password_from_db:
+                if checkbx[0] != isLoged_from_db:
+                    print("All good")
+                    if checkbx[0] == 'Staying loged in':
+                        mycurs.execute("UPDATE user_db SET is_loged = 'Staying loged in' WHERE ip = %s",(ip_addres,))
+                        conn.commit()    
+            else:
+                put_markdown("Your login or password is incorrect(if you don't have account,press upper button)")
+        except Exception as e:
+            if nickname == name_from_db and password == password_from_db and repr(e) == "IndexError('list index out of range')": 
+                mycurs.execute("UPDATE user_db SET is_loged = 'No' WHERE ip = %s",(ip_addres,))
+                conn.commit() 
 
 conn.close()
 
